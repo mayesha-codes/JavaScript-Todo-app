a54e152ac4fe9c4d4392176b8b876a0f8d612aa0
mayesha-codes <maisha.tasnim.247@gmail.com>
2026-07-13 21:18:06 +0600

    list item styles updated

--- js/ui.js
+++ js/ui.js
@@ -1,9 +1,26 @@
 const taskList = document.getElementById("task-list");
 
+function renderTask(task) {
+  return `<li class="task">
+             <label class="task__check">
+                 <input type="checkbox"  data-task-id="${task.id}">
+                 <span class="task__box"></span>
+             </label>
+
+             <span class="task__text">${task.text}</span>
+
+             <div class="task__actions">
+                 <button class="icon-btn icon-btn--edit">Edit</button>
+                 <button class="icon-btn icon-btn--delete">Delete</button>
+             </div>
+         </li>`;
+}
+
 export function renderTasks(tasks) {
   let listItems = "";
   for (const task of tasks) {
-    listItems += `<li>${task.text}</li>`;
+    listItems += renderTask(task);
+    
   }
   taskList.innerHTML=listItems;
 }
